349910a7200505aaba2588f928b97798d51c015c
mariaangulo98 <47619708+mariaangulo98@users.noreply.github.com>
2020-09-18 23:28:30 -0600

    Add files via upload

--- Análisis de Datos/Funciones/Features_edf.m
+++ Análisis de Datos/Funciones/Features_edf.m
@@ -2,16 +2,30 @@
 % a partir de un eeg, pueden analizarse hasta 4 canales.
 % Se devuelve la matrix de features, la matriz de data procesada y
 % los canales que se analizaron.
-function [Matriz_features,channel_ventana,c] = Features_edf(edf,canales,muestras)
+%edf debe ingresarse en matriz de forma canales x muestras
+function [Matriz_features,channel_ventana,c] = Features_edf(edf,canales,muestras,c,op)
 % ARGUMENTOS DE LA FUNCION
 %edf:   archivo .edf con señal EEG a analizar
 %canales: número de canales para encontrar features (1-4)
 %muestras: número de muestras para realizar ventanas
-
+%op=0 MAV,ZC    op=1 varianza, curtosis, mav
+%arreglar dimension
+if size(edf,1)>size(edf,2)
+    edf = edf';
+end
+ctot = size(edf,1);
+%eeg = zeros(ctot,length(edf));
+if c==0
 %Definir canales a analizar
-for i=1:canales
-    c(i)= randi([1 23]);
-    eeg(:,i)= edf(i,:)';
+    comb = combnk(1:ctot,canales);
+    x = randi([1 length(comb)],1);
+    for i=1:canales
+        eeg(:,i)= edf(comb(x,i),:)';
+    end
+else
+    for i=1:canales
+        eeg(:,i)= edf(c(i),:)';
+    end
 end
 %Preprocesamiento: FILTROS
 Fs = 256;
@@ -24,12 +38,13 @@ fruido = designfilt('bandstopiir','FilterOrder',2, ...
 lowpass = designfilt('lowpassiir','FilterOrder',8, ...
          'PassbandFrequency',49,'PassbandRipple',0.2, ...
          'SampleRate',Fs);
-channels = filtfilt(fred_electrica,eeg);
-
-%for i=1:canales
-    %Normalizar señales
-%    channels(:,i) = channels/max(channels(:,i));
-%end
+channels = filtfilt(fred_electrica,eeg);    
+%channels = filtfilt(lowpass,eeg);
+%channels = eeg;
+%  for i=1:canales
+%      %Normalizar señales
+%      channels(:,i) = channels(:,i)/max(abs(channels(:,i)));
+%  end
 %Realizar ventana
 k=1;        %recorrer canales
 i=1;        %recorrer muestras
@@ -37,27 +52,17 @@ j=0;
 flag=0;
 size_c = length(channels);
 channel_ventana = zeros(size_c,canales);
-zc = zeros(round(size_c/muestras)-1,canales);
-mav = zeros(round(size_c/muestras)-1,canales);
-%varianza = zeros(round(size_c/muestras)-1,canales);
+z = zeros(length(eeg),canales);
+zc = zeros(round(size_c/muestras),canales);
+mav = zeros(round(size_c/muestras),canales);
+curtosis = zeros(round(size_c/muestras),canales);
+desviacion = zeros(round(size_c/muestras),canales);
 %zero crossing index function
-umbral = 0.005;
-if canales==1
-    z1 = ZC(channels(:,1),umbral)'; %Calcular todos los ZC de la señal
-elseif canales==2
-    z1 = ZC(channels(:,1),umbral)';
-    z2 = ZC(channels(:,2),umbral)'; 
-elseif canales==3
-    z1 = ZC(channels(:,1),umbral)';
-    z2 = ZC(channels(:,2),umbral)'; 
-    z3 = ZC(channels(:,3),umbral)'; 
-elseif canales==4
-    z1 = ZC(channels(:,1),umbral)';
-    z2 = ZC(channels(:,2),umbral)'; 
-    z3 = ZC(channels(:,3),umbral)'; 
-    z4 = ZC(channels(:,4),umbral)'; 
+max_amplitud = max(abs(channels))*0.02; % 2% de la amplitud de la señal 
+umbral = max_amplitud(1,1);
+for i=1:canales
+   z(:,i) =  ZC(channels(:,i),umbral)'; %Calcular todos los ZC de la señal
 end
-
 while(1)
      
      channel_ventana(i,k) = channels(i,k); 
@@ -65,43 +70,19 @@ while(1)
         if(mod(i,(muestras+1))==0)      %Calcular caracteristicas de cada ventana
             flag = flag+1;
             mav(flag,k) = mean(abs(channel_ventana(:,k)));
-           %varianza(flag,k) = var(channel_ventana(:,k));
-           % picos = findpeaks(channel_ventana(:,k)');
+            curtosis(flag,k) = kurtosis(channel_ventana(:,k));
+            desviacion(flag,k) = std(channel_ventana(:,k));
             if(j>0) %contar ZC por cada ventana
-                if canales==1
-                    zc(flag,1) = sum(z1(j*muestras:i) == 1);
-                elseif canales==2
-                    zc(flag,1) = sum(z1(j*muestras:i) == 1);
-                    zc(flag,2) = sum(z2(j*muestras:i) == 1);
-                elseif canales==3
-                    zc(flag,1) = sum(z1(j*muestras:i) == 1);
-                    zc(flag,2) = sum(z2(j*muestras:i) == 1);
-                    zc(flag,3) = sum(z3(j*muestras:i) == 1);
-                elseif canales==4
-                    zc(flag,1) = sum(z1(j*muestras:i) == 1);
-                    zc(flag,2) = sum(z2(j*muestras:i) == 1);
-                    zc(flag,3) = sum(z3(j*muestras:i) == 1);
-                    zc(flag,4) = sum(z4(j*muestras:i) == 1);
-                end
+                for o=1:canales
+                    zc(flag,o) = sum(z(j*muestras:i,o) == 1);
+                end 
             else
-                if canales==1
-                    zc(flag,1) = sum(z1(1:(i-1)) == 1);
-                elseif canales==2
-                    zc(flag,1) = sum(z1(1:(i-1)) == 1);
-                    zc(flag,2) = sum(z2(1:(i-1)) == 1);
-                elseif canales==3
-                    zc(flag,1) = sum(z1(1:(i-1)) == 1);
-                    zc(flag,2) = sum(z2(1:(i-1)) == 1);
-                    zc(flag,3) = sum(z3(1:(i-1)) == 1);
-                elseif canales==4
-                    zc(flag,1) = sum(z1(1:(i-1)) == 1);
-                    zc(flag,2) = sum(z2(1:(i-1)) == 1);
-                    zc(flag,3) = sum(z3(1:(i-1)) == 1);
-                    zc(flag,4) = sum(z4(1:(i-1)) == 1);
-                end
+               for o=1:canales
+                     zc(flag,o) = sum(z(1:(i-1),o) == 1);
+                end 
             end
             j= j+1;   
-           % fmax(flag,k)=max(picos);
+            %fmax(flag,k)=max(picos);
         end
      if (i==size_c) 
             k=k+1;
@@ -114,16 +95,18 @@ while(1)
      end
 end
 %Concatenar vector de características
-    Matriz_features(:,1:2) = [mav(:,1),zc(:,1)];
-if canales==2
-    Matriz_features(:,3:4) = [mav(:,2),zc(:,2)];
-elseif canales==3
-    Matriz_features(:,3:4) = [mav(:,2),zc(:,2)];
-    Matriz_features(:,5:6) = [mav(:,3),zc(:,3)];
-elseif canales==4
-    Matriz_features(:,3:4) = [mav(:,2),zc(:,2)];
-    Matriz_features(:,5:6) = [mav(:,3),zc(:,3)];
-    Matriz_features(:,7:8) = [mav(:,4),zc(:,4)];
+a=0;
+if op ==0
+for i=1:canales
+    Matriz_features(:,i+a:((i+a)+1)) = [mav(:,i),zc(:,i)];
+    a=a+1;
 end
+elseif op==1
+    for i=1:canales
+    Matriz_features(:,i+a:((i+a)+3)) = [zc(:,i),mav(:,i),curtosis(:,i),desviacion(:,i)];
+    a=a+3;
+    end
+end
+
 end
 
